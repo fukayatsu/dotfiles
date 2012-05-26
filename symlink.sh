113726746814d3da28b8ca6ee875a1d102e41e14
fukayatsu <fukayatsu@gmail.com>
2012-05-27 01:05:43 +0900

    modify xmodmaprc for ubuntu on MacBook Air

--- symlink.sh
+++ symlink.sh
@@ -2,7 +2,8 @@
 ln -fs $PWD/gitignore $HOME/.gitignore
 ln -fs $PWD/vimrc $HOME/.vimrc
 ln -fs $PWD/gvimrc $HOME/.gvimrc
-ln -fx $PWD/viablerc $HOME/.viablerc
+ln -fs $PWD/viablerc $HOME/.viablerc
+ln -fs $PWD/xmodmaprc $HOME/.xmodmaprc
 
 ln -fs $PWD/zshrc $HOME/.zshrc
 ln -fs $PWD/bashrc $HOME/.bashrc
