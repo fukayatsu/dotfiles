14e1b439bc8de491ed89a9f5056cd3a0422e3711
fukayatsu <fukayatsu@gmail.com>
2022-07-17 10:53:03 +0900

    Tweak mac defaults

--- macos/30_defaults_write.sh
+++ macos/30_defaults_write.sh
@@ -6,4 +6,14 @@ set -eu
 defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter Bluetooth -int 18
 defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter Sound -int 18
 
-# TODO
+# スクリーンショットのファイル名を変更
+defaults write com.apple.screencapture name "ss"
+
+# トラックパッド
+defaults write -g com.apple.trackpad.scaling 2.5
+defaults write -g com.apple.mouse.tapBehavior -int 1
+
+# キーリピートを有効化・高速化
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g KeyRepeat -int 3
+defaults write -g InitialKeyRepeat -int 10
