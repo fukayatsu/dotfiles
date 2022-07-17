87295f307a40b31bd28437bd89a79407d8fe62af
fukayatsu <fukayatsu@gmail.com>
2022-07-17 18:22:02 +0900

    Add karabiner.json

--- install.sh
+++ install.sh
@@ -3,3 +3,11 @@
 set -eu
 
 cp -a src/common/.[^.]* $HOME/
+
+if [ $(uname -s) = "Linux" ]; then
+  echo 'setup for Linux'
+elif [ $(uname -s) = "Darwin" ]; then
+  echo 'setup for macOS'
+
+  cp -a src/macos/.config/* $HOME/.config/
+fi
