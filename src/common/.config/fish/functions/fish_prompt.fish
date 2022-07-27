e5e2438399ab31004d7f4c7217e01baf6f5e3a70
fukayatsu <fukayatsu@gmail.com>
2022-07-27 13:52:55 +0900

    Show prompt_login

--- src/common/.config/fish/functions/fish_prompt.fish
+++ src/common/.config/fish/functions/fish_prompt.fish
@@ -69,6 +69,9 @@ function fish_prompt --description 'Write out the prompt'
             set suffix '$'
     end
 
+    # USER@HOST
+    echo -n (prompt_login)' '
+
     # PWD
     set_color $color_cwd
     echo -n (prompt_pwd)
