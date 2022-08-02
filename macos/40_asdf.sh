0101b159939be90b388fc28b497f92df2c728134
fukayatsu <fukayatsu@gmail.com>
2022-08-02 16:41:49 +0900

    Tweak asdf commands

--- macos/40_asdf.sh
+++ macos/40_asdf.sh
@@ -4,9 +4,9 @@ set -eu
 
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 # https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
-# asdf list-all ruby
-# asdf install ruby latest
-# asdf global ruby latest
+asdf install ruby latest
+asdf global ruby latest
 
 asdf plugin add fzf https://github.com/kompiro/asdf-fzf.git
-
+asdf install fzf latest
+asdf global fzf latest
