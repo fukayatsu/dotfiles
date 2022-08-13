dd1fed62d1d3010a2dbcfc17d3901e2cc0a189cc
fukayatsu <fukayatsu@gmail.com>
2022-08-13 14:43:07 +0900

    Tweak ghq key binding

--- src/common/.config/fish/conf.d/ghq_key_bindings.fish
+++ src/common/.config/fish/conf.d/ghq_key_bindings.fish
@@ -1,4 +1,6 @@
-bind \cg '__ghq_repository_search'
-if bind -M insert >/dev/null 2>/dev/null
-    bind -M insert \cg '__ghq_repository_search'
+if type -q ghq
+    bind \cg '__ghq_repository_search'
+    if bind -M insert >/dev/null 2>/dev/null
+        bind -M insert \cg '__ghq_repository_search'
+    end
 end
